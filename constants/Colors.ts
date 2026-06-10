41736b401dccc76d493c424a43359624d65d844d
Ravi Rai <ravirairgstartup@gmail.com>
2026-06-10 14:29:00 +0530

    fix: add missing constants/Colors.ts (PRIMARY_COLOR, SECONDARY_COLOR)

---
+++ constants/Colors.ts
@@ -0,0 +1,22 @@
+// App color constants
+export const PRIMARY_COLOR = '#F68537';
+export const SECONDARY_COLOR = '#EBD8B7';
+
+export const Colors = {
+  light: {
+    text: '#11181C',
+    background: '#fff',
+    tint: PRIMARY_COLOR,
+    icon: '#687076',
+    tabIconDefault: '#687076',
+    tabIconSelected: PRIMARY_COLOR,
+  },
+  dark: {
+    text: '#ECEDEE',
+    background: '#151718',
+    tint: '#fff',
+    icon: '#9BA1A6',
+    tabIconDefault: '#9BA1A6',
+    tabIconSelected: '#fff',
+  },
+};
